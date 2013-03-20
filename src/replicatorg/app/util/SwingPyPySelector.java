65d4abf2d82b3da0d7e02696bd9edef7aae72363
Dan Newman <dan.newman@mtbaldy.us>
2013-03-20 13:05:09 -0700

    Work on Windows hack

---
+++ src/replicatorg/app/util/SwingPyPySelector.java
@@ -0,0 +1,44 @@
+package replicatorg.app.util;
+
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.ListSelectionModel;
+
+import net.miginfocom.swing.MigLayout;
+import replicatorg.app.Base;
+import replicatorg.app.util.PythonUtils.Selector;
+
+public class SwingPyPySelector implements Selector {
+	private Frame frame;
+	
+	public String selectPythonPath(Vector<String> candidates) { return null; }
+
+	public SwingPyPySelector(Frame frame) {
+		this.frame = frame;
+	}
+
+	public String selectFreeformPath() {
+		JFileChooser chooser = new JFileChooser();
+		chooser.setDialogTitle("Select installed PyPy binary");
+		chooser.setDialogType(JFileChooser.OPEN_DIALOG);
+		if (chooser.showOpenDialog(this.frame) == JFileChooser.APPROVE_OPTION) {
+			File chosen = chooser.getSelectedFile();
+			if (chosen != null) {
+				return chosen.getAbsolutePath();
+			}
+		}
+		return null;
+	}
+
+}
